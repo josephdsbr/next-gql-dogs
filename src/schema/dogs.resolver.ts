a866945a1412d43802e16f4d2c2ad43ac52f001c
josephdsbr@gmail.com <josephdsbr@gmail.com>
2022-07-06 08:23:24 -0300

    finishing integration with our graphql server

--- src/schema/dogs.resolver.ts
+++ src/schema/dogs.resolver.ts
@@ -6,6 +6,17 @@ import dogs from './dogs.json'
 
 @Resolver()
 export class DogsResolver {
+    @Query(() => Dog, { nullable: true })
+    dog(@Arg("name", () => String) name: string): Dog | undefined {
+        const dog =  dogs.find((dog) => dog.name === name);
+
+        if (!dog) {
+            throw new Error("Dog not found")
+        }
+
+        return dog;
+    }
+
     @Query(() => [Dog])
     dogs(): Dog[] {
         return dogs;
